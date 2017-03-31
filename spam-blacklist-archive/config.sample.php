cd684abd97dc90c0622233139f8f7ab305d46859
xiplus <huangxuanyuxiplus@gmail.com>
2017-04-01 07:13:37 +0800

    update config sample

--- spam-blacklist-archive/config.sample.php
+++ spam-blacklist-archive/config.sample.php
@@ -4,11 +4,18 @@ $C["wikiapi"] = "https://zh.wikipedia.org/w/api.php";
 $C["user"] = "";
 $C["pass"] = "";
 
-$C["archive_ago"] = 60*60*24*7;
-$C["fail_retry"] = 5;
-
 $C["from_page"] = "MediaWiki_talk:Spam-blacklist";
 $C["to_page_prefix"] = "MediaWiki_talk:Spam-blacklist/存档/";
+
+$C["retention_time"] = "https://zh.wikipedia.org/wiki/User:A2093064-bot/task/1/config/retention_time?action=raw";
+$C["retention_time_default"] = 604800;
+$C["retention_bytes"] = "https://zh.wikipedia.org/wiki/User:A2093064-bot/task/1/config/retention_bytes?action=raw";
+$C["retention_bytes_default"] = 1000000;
+
+$C["fail_retry"] = 5;
+
+$C["summary_prefix"] = "[[User:A2093064-bot/task/1|機器人]]";
+$C["summary_config_page"] = "[[User:A2093064-bot/task/1/config|門檻]]";
 
 $C["User-Agent"] = "User:A2093064-bot spam-blacklist-archive";
 
