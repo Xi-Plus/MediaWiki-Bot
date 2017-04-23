f65a88bc424c4d48446aee6819bf448313bf8255
xiplus <huangxuanyuxiplus@gmail.com>
2017-04-23 09:44:46 +0800

    add fix-non-free-files-backlink

---
+++ fix-non-free-files-backlink/config.sample.php
@@ -0,0 +1,18 @@
+<?php
+
+$C["wikiapi"] = "https://zh.wikipedia.org/w/api.php";
+$C["user"] = "";
+$C["pass"] = "";
+
+$C["category"] = array(
+	"Category:红链的非自由图像",
+	"Category:缺少回链的非自由图像"
+);
+
+$C["fail_retry"] = 5;
+
+$C["summary_prefix"] = "[[User:A2093064-bot/task/8|機器人]]";
+
+$C["User-Agent"] = "User:A2093064-bot fix-non-free-files-backlink";
+
+$C["cookiefile"] = __DIR__."/../tmp/fix-non-free-files-backlink-cookie.txt";
---
+++ fix-non-free-files-backlink/edit.php
@@ -0,0 +1,108 @@
+<?php
+require(__DIR__."/../config/config.php");
+if (!in_array(PHP_SAPI, $C["allowsapi"])) {
+	exit("No permission");
+}
+
+set_time_limit(600);
+date_default_timezone_set('UTC');
+$starttime = microtime(true);
+@include(__DIR__."/config.php");
+require(__DIR__."/../function/curl.php");
+require(__DIR__."/../function/login.php");
+require(__DIR__."/../function/edittoken.php");
+
+echo "The time now is ".date("Y-m-d H:i:s")." (UTC)\n";
+
+login();
+$edittoken = edittoken();
+
+foreach ($C["category"] as $category) {
+	$res = cURL($C["wikiapi"]."?".http_build_query(array(
+		"action" => "query",
+		"format" => "json",
+		"list" => "categorymembers",
+		"cmtitle" => $category,
+		"cmnamespace" => "6",
+		"cmlimit" => "max"
+	)));
+	if ($res === false) {
+		exit("fetch page fail\n");
+	}
+	$res = json_decode($res, true);
+	$pagelist = $res["query"]["categorymembers"];
+	foreach ($pagelist as $page) {
+		echo $page["title"]."\n";
+		$res = cURL($C["wikiapi"]."?".http_build_query(array(
+			"action" => "query",
+			"format" => "json",
+			"prop" => "fileusage",
+			"pageids" => $page["pageid"]
+		)));
+		if ($res === false) {
+			exit("fetch page fail\n");
+		}
+		$res = json_decode($res, true);
+		$pages = current($res["query"]["pages"]);
+		if (count($pages["fileusage"]) > 1) {
+			echo "use more than 1\n";
+			continue;
+		}
+		$article = $pages["fileusage"][0]["title"];
+		echo $article."\n";
+		for ($i=$C["fail_retry"]; $i > 0; $i--) {
+			$starttimestamp = time();
+			$res = cURL($C["wikiapi"]."?".http_build_query(array(
+				"action" => "query",
+				"prop" => "revisions",
+				"format" => "json",
+				"rvprop" => "content|timestamp",
+				"pageids" => $page["pageid"]
+			)));
+			if ($res === false) {
+				exit("fetch page fail\n");
+			}
+			$res = json_decode($res, true);
+			$pages = current($res["query"]["pages"]);
+			$text = $pages["revisions"][0]["*"];
+			$basetimestamp = $pages["revisions"][0]["timestamp"];
+
+			if (preg_match_all("/^ *\| *Article *= *.+$/mi", $text, $m) !== 1) {
+				echo "not match 1 time\n";
+				continue;
+			}
+
+			$text = preg_replace("/^( *\| *Article *=).+$/mi", '${1} '.$article, $text);
+
+			$summary = $C["summary_prefix"]."，修正[[:".$category."]]";
+			$post = array(
+				"action" => "edit",
+				"format" => "json",
+				"pageid" => $page["pageid"],
+				"summary" => $summary,
+				"text" => $text,
+				"token" => $edittoken,
+				"minor" => "",
+				"starttimestamp" => $starttimestamp,
+				"basetimestamp" => $basetimestamp
+			);
+			echo "edit ".$page["title"]." summary=".$summary."\n";
+			if (!$C["test"]) $res = cURL($C["wikiapi"], $post);
+			else $res = false;
+			$res = json_decode($res, true);
+			if (isset($res["error"])) {
+				echo "edit fail\n";
+				if ($i === 1) {
+					exit("quit\n");
+				} else {
+					echo "retry\n";
+				}
+			} else {
+				break;
+			}
+		}
+	}
+}
+
+$spendtime = (microtime(true)-$starttime);
+echo "spend ".$spendtime." s.\n";
