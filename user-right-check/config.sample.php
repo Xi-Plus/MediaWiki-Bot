8261932979d7f662fca10fb89c58d16ca0f0bcde
xiplus <huangxuanyuxiplus@gmail.com>
2017-06-08 14:24:18 +0800

    URC config update & database

--- user-right-check/config.sample.php
+++ user-right-check/config.sample.php
@@ -4,6 +4,8 @@ $C["wikiapi"] = "https://zh.wikipedia.org/w/api.php";
 $C["user"] = "";
 $C["pass"] = "";
 
+$C["day"] = array("日", "一", "二", "三", "四", "五", "六");
+
 $C["User-Agent"] = "User:A2093064-bot user-right-check";
 
 $C["DBTBprefix"] = "user_right_check_";
---
+++ user-right-check/database.sql
@@ -0,0 +1,53 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+CREATE TABLE `user_right_check_botlist` (
+  `botid` int(11) NOT NULL,
+  `botname` varchar(255) NOT NULL,
+  `botlastedit` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `botlastlog` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `botrights` varchar(255) NOT NULL DEFAULT '',
+  `userid` int(11) NOT NULL DEFAULT '0',
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `userlastedit` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `userlastlog` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `reported` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_right_check_log` (
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `message` text NOT NULL,
+  `hash` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_right_check_userlist` (
+  `userid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `lastedit` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `lastlog` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `lastusergetrights` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `rights` varchar(255) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `user_right_check_botlist`
+  ADD UNIQUE KEY `userid` (`botid`);
+
+ALTER TABLE `user_right_check_log`
+  ADD UNIQUE KEY `hash` (`hash`);
+
+ALTER TABLE `user_right_check_userlist`
+  ADD UNIQUE KEY `userid` (`userid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
