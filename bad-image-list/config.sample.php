f0bd11d514da46ad18e3e66776a319bc3fcec2a5
xiplus <huangxuanyuxiplus@gmail.com>
2017-07-11 21:30:12 +0800

    bad-image-list

---
+++ bad-image-list/config.sample.php
@@ -0,0 +1,20 @@
+<?php
+
+$C["wikiapi"] = "https://zh.wikipedia.org/w/api.php";
+$C["user"] = "";
+$C["pass"] = "";
+
+$C["page"] = "https://zh.wikipedia.org/wiki/MediaWiki:Bad_image_list?action=raw";
+$C["page2"] = "https://en.wikipedia.org/wiki/MediaWiki:Bad_image_list?action=raw";
+
+$C["text1"] = "以下内容复制自英文维基百科";
+
+$C["outpage"] = "User:A2093064-bot/task/10/output/MediaWiki:Bad image list";
+
+$C["fail_retry"] = 5;
+
+$C["summary_prefix"] = "[[User:A2093064-bot/task/10|機器人10]]：產生[[MediaWiki:Bad image list]]：";
+
+$C["User-Agent"] = "User:A2093064-bot bad-image-list";
+
+$C["cookiefile"] = __DIR__."/../tmp/bad-image-list-cookie.txt";
---
+++ bad-image-list/edit.php
@@ -0,0 +1,114 @@
+<?php
+require(__DIR__."/../config/config.php");
+if (!in_array(PHP_SAPI, $C["allowsapi"])) {
+	exit("No permission");
+}
+
+set_time_limit(600);
+date_default_timezone_set('UTC');
+$starttime = microtime(true);
+@include(__DIR__."/config.php");
+require(__DIR__."/../function/curl.php");
+require(__DIR__."/../function/login.php");
+require(__DIR__."/../function/edittoken.php");
+
+echo "The time now is ".date("Y-m-d H:i:s")." (UTC)\n";
+
+login();
+$edittoken = edittoken();
+
+for ($i=0; $i < $C["fail_retry"]; $i++) { 
+	$text = file_get_contents($C["page"]);
+	if ($text === false) {
+		echo "fetch fail\n";
+		continue;
+	}
+	$entext = file_get_contents($C["page2"]);
+	if ($entext === false) {
+		echo "fetch fail\n";
+		continue;
+	}
+
+	$hash = md5(uniqid(rand(), true));
+	echo "hash ".$hash."\n";
+	$text = preg_replace("/^(\* \[\[:File:.+?)\n([^*])/m", "$1".$hash."$2", $text);
+	$text = explode($hash, $text);
+	echo "find ".count($text)." sections\n";
+	if (count($text) != 3) {
+		echo "split fail\n";
+		continue;
+	}
+
+	$exist = [];
+	$out = "";
+	foreach ($text as $temp) {
+		if (strpos($temp, $C["text1"]) !== false) {
+			$out .= "\n";
+			$temp = explode("\n", $temp);
+			$except = [];
+			foreach ($temp as $temp2) {
+				if (preg_match("/^\* \[\[(:File:[^]]+)\]\](.*)$/i", $temp2, $m)) {
+					$file = $m[1];
+					$file = str_replace("_", " ", $file);
+					$except[$file] = [];
+					if (preg_match_all("/\[\[([^]]+?)\]\]/", $m[2], $m2)) {
+						foreach ($m2[1] as $page) {
+							$page = str_replace("_", " ", $page);
+							$except[$file] []= $page;
+						}
+					}
+				} else {
+					$out .= "\n".$temp2."\n";
+				}
+			}
+			preg_match_all("/^\* \[\[(:File:[^]]+)/mi", $entext, $m);
+			foreach ($m[1] as $file) {
+				$file = str_replace("_", " ", $file);
+				$out .= "* [[".$file."]]";
+				if (isset($except[$file]) && count($except[$file]) > 0) {
+					$out .= " except on [[".implode("]], [[", $except[$file])."]]";
+				}
+				$out .= "\n";
+				unset($except[$file]);
+			}
+			$out .= "\n";
+		} else {
+			$out .= "\n".$temp."\n";
+		}
+	}
+	$out = preg_replace("/^\n+/", "", $out);
+	$out = preg_replace("/\n+$/", "", $out);
+	$out = preg_replace("/\n{3,}/", "\n\n", $out);
+	$date = date("Y/m/d");
+	$out = preg_replace("/更新至\d+\/\d+\/\d+/", "更新至".$date, $out);
+
+	$summary = $C["summary_prefix"]."更新";
+	$post = array(
+		"action" => "edit",
+		"format" => "json",
+		"title" => $C["outpage"],
+		"summary" => $summary,
+		"text" => $out,
+		"token" => $edittoken
+	);
+	echo "edit ".$C["outpage"]." summary=".$summary."\n";
+	if (!$C["test"]) {
+		$res = cURL($C["wikiapi"], $post);
+	} else {
+		$res = false;
+		file_put_contents("out.txt", $out);
+	}
+	$res = json_decode($res, true);
+	if (isset($res["error"])) {
+		echo "edit fail\n";
+		continue;
+	}
+
+	$remove = "";
+	foreach ($except as $page => $temp) {
+		$remove .= "* [[".$page."]]\n";
+	}
+	file_put_contents("remove.txt", $remove);
+
+	break;
+}
