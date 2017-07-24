7a8c0ce03625f681758a956442fea6efd1ca507e
xiplus <huangxuanyuxiplus@gmail.com>
2017-07-24 13:02:40 +0800

    Newusermessage-signatures

---
+++ Newusermessage-signatures/config.sample.php
@@ -0,0 +1,17 @@
+<?php
+
+$C["wikiapi"] = "https://zh.wikipedia.org/w/api.php";
+$C["user"] = "";
+$C["pass"] = "";
+
+$C["page"] = "https://zh.wikipedia.org/wiki/MediaWiki:Newusermessage-signatures?action=raw";
+
+$C["outpage"] = "User:A2093064-bot/task/11/output/status";
+
+$C["fail_retry"] = 5;
+
+$C["summary_prefix"] = "[[User:A2093064-bot/task/11|機器人11]]：檢查[[MediaWiki:Newusermessage-signatures]]：";
+
+$C["User-Agent"] = "User:A2093064-bot Newusermessage-signatures";
+
+$C["cookiefile"] = __DIR__."/../tmp/Newusermessage-signatures-cookie.txt";
---
+++ Newusermessage-signatures/edit.php
@@ -0,0 +1,85 @@
+<?php
+require(__DIR__."/../config/config.php");
+if (!in_array(PHP_SAPI, $C["allowsapi"])) {
+	exit("No permission");
+}
+
+set_time_limit(600);
+date_default_timezone_set('UTC');
+$starttime = microtime(true);
+@include(__DIR__."/config.php");
+require(__DIR__."/../function/curl.php");
+require(__DIR__."/../function/login.php");
+require(__DIR__."/../function/edittoken.php");
+
+echo "The time now is ".date("Y-m-d H:i:s")." (UTC)\n";
+
+login();
+$edittoken = edittoken();
+
+for ($i=0; $i < $C["fail_retry"]; $i++) { 
+	$text = file_get_contents($C["page"]);
+	if ($text === false) {
+		echo "fetch fail\n";
+		continue;
+	}
+
+	preg_match_all("/^(\* *|: inactive\|)(.+)$/m", $text, $m);
+	$out = '{| class="wikitable sortable"
+!使用者
+!簽名
+!狀態
+!字數
+!位元組
+';
+	foreach ($m[0] as $key => $temp) {
+		$status = $m[1][$key];
+		if (strpos($status, "inactive") !== false) {
+			$status = "inactive";
+		} else {
+			$status = "";
+		}
+		$sign = $m[2][$key];
+		$user = "";
+		if (preg_match_all("/\[\[(?:User:|用户:|User talk:|User_talk:|用户讨论:|Special:Contributions\/|Special:用户贡献\/|Special:用戶貢獻\/|特殊:用户贡献\/|特殊:用戶貢獻\/)([^|\/]+)/i", $sign, $m2)) {
+			$user = "{{User|".$m2[1][0]."}}";
+		}
+		$len = mb_strlen($sign);
+		$byte = strlen($sign);
+		if ($byte > 255) {
+			$byte = "{{red|'''".$byte."'''}}";
+		}
+		$out .= '|-
+|'.$user.'
+| '.$sign.'
+|'.$status.'
+|'.$len.'
+|'.$byte.'
+';
+	}
+	$out .= '|}';
+
+	$summary = $C["summary_prefix"]."更新";
+	$post = array(
+		"action" => "edit",
+		"format" => "json",
+		"title" => $C["outpage"],
+		"summary" => $summary,
+		"text" => $out,
+		"token" => $edittoken
+	);
+	echo "edit ".$C["outpage"]." summary=".$summary."\n";
+	if (!$C["test"]) {
+		$res = cURL($C["wikiapi"], $post);
+	} else {
+		$res = false;
+		file_put_contents("out.txt", $out);
+	}
+	$res = json_decode($res, true);
+	if (isset($res["error"])) {
+		echo "edit fail\n";
+		continue;
+	}
+
+	break;
+}
