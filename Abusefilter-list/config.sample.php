3df1a9042f95440f80ed7d9c6dce22a659b54cc8
xiplus <huangxuanyuxiplus@gmail.com>
2017-10-15 20:01:46 +0800

    +abusefilter list

---
+++ Abusefilter-list/config.sample.php
@@ -0,0 +1,13 @@
+<?php
+
+$C["wikiapi"] = "https://zh.wikipedia.org/w/api.php";
+$C["user"] = "";
+$C["pass"] = "";
+
+$C["outpage"] = "User:A2093064/AF";
+
+$C["summary_prefix"] = "產生防濫用過濾器列表：";
+
+$C["User-Agent"] = "";
+
+$C["cookiefile"] = __DIR__."/../tmp/abusefilter-list-cookie.txt";
---
+++ Abusefilter-list/edit.php
@@ -0,0 +1,79 @@
+<?php
+require(__DIR__."/../config/config.php");
+if (!in_array(PHP_SAPI, $C["allowsapi"])) {
+	exit("No permission");
+}
+
+set_time_limit(600);
+date_default_timezone_set('UTC');
+$starttime = microtime(true);
+@include(__DIR__."/config.php");
+require(__DIR__."/../function/curl.php");
+require(__DIR__."/../function/login.php");
+require(__DIR__."/../function/edittoken.php");
+
+echo "The time now is ".date("Y-m-d H:i:s")." (UTC)\n";
+
+login();
+$edittoken = edittoken();
+
+$res = cURL($C["wikiapi"]."?".http_build_query(array(
+	"action" => "query",
+	"format" => "json",
+	"list" => "abusefilters",
+	"abflimit" => "max",
+	"abfprop" => "id|description|actions|status|private|hits"
+)));
+if ($res === false) {
+	exit("fetch page fail\n");
+}
+$res = json_decode($res, true);
+
+$out = '{| class="wikitable sortable" style="background-color: #fff;"
+|- 
+! style="background-color: #ddf;"| {{int:abusefilter-list-id}}
+! style="background-color: #ddf;"| {{int:abusefilter-list-public}}
+! style="background-color: #ddf;"| {{int:abusefilter-list-consequences}}
+! style="background-color: #ddf;"| {{int:abusefilter-list-status}}
+! style="background-color: #ddf;"| {{int:abusefilter-list-visibility}}
+! style="background-color: #ddf;"| {{int:abusefilter-list-hitcount}}
+';
+foreach ($res["query"]["abusefilters"] as $AF) {
+	$action = $AF["actions"];
+	$action = str_replace("warn", "{{int:abusefilter-action-warn}}", $action);
+	$action = str_replace("tag", "{{int:abusefilter-action-tag}}", $action);
+	$action = str_replace("disallow", "{{int:abusefilter-action-disallow}}", $action);
+	$action = str_replace("throttle", "{{int:abusefilter-action-throttle}}", $action);
+	$action = str_replace(",", "、", $action);
+	$out .= '|- style="color: '.(isset($AF["enabled"])?"#000":(isset($AF["deleted"])?"#aaa":"#666")).';"
+| [[Special:AbuseFilter/'.$AF["id"].'|'.$AF["id"].']]
+| [[Special:AbuseFilter/'.$AF["id"].'|'.$AF["description"].']]
+|'.$action.'
+|'.(isset($AF["enabled"])?"{{int:abusefilter-enabled}}":(isset($AF["deleted"])?"{{int:abusefilter-deleted}}":"{{int:abusefilter-disabled}}")).'
+|'.(isset($AF["private"])?"{{int:abusefilter-hidden}}":"{{int:abusefilter-unhidden}}").'
+|data-sort-value='.$AF["hits"].'| [{{fullurl:Special:AbuseLog|wpSearchFilter='.$AF["id"].'}} {{int:abusefilter-hitcount|'.$AF["hits"].'}}]
+';
+}
+$out .= '|}';
+
+$summary = $C["summary_prefix"]."更新";
+$post = array(
+	"action" => "edit",
+	"format" => "json",
+	"title" => $C["outpage"],
+	"summary" => $summary,
+	"text" => $out,
+	"minor" => "",
+	"token" => $edittoken
+);
+echo "edit ".$C["outpage"]." summary=".$summary."\n";
+if (!$C["test"]) {
+	$res = cURL($C["wikiapi"], $post);
+} else {
+	$res = false;
+	file_put_contents(__DIR__."/out.txt", $out);
+}
+$res = json_decode($res, true);
+if (isset($res["error"])) {
+	echo "edit fail\n";
+}
