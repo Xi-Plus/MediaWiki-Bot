4e02bec91e293b4dac59ed9006caabd38d152701
xiplus <huangxuanyuxiplus@gmail.com>
2018-01-01 21:40:16 +0800

    adminstats add zero edits

--- adminstats/fetch.php
+++ adminstats/fetch.php
@@ -13,7 +13,7 @@ $time = time();
 echo "The time now is ".date("Y-m-d H:i:s", $time)." (UTC)\n";
 
 $dates = ["-1 month", "-3 months", "-6 months", "-1 year"];
-$counts = [10, 100, 500, 1000];
+$counts = [1, 10, 100, 500, 1000];
 $res = [];
 foreach ($dates as $date) {
 	$realdate = date("Y-m-d", strtotime($date));
@@ -48,22 +48,29 @@ if (preg_match("/<a href='https:\/\/zh\.wikipedia\.org\/w\/index\.php\?title=Spe
 $out = "==統計==
 *更新日期：".date("Y年m月d日", $time)." (".$C["day"][date("w", $time)].") ".date("H:i", $time)." (UTC)
 *中文維基管理員：".$admincount."人
+*備註：由[https://xtools.wmflabs.org/adminstats/zh.wikipedia.org XTools]協助統計，只計算刪除、封禁、保護、匯入、權限，因此可能與實際情況有誤差。
 
-===管理方面編輯次數少於10次===
-*一年：".$res[10]["-1 year"]."人（".round(100*$res[10]["-1 year"]/$admincount)."%）
-*半年：".$res[10]["-6 months"]."人（".round(100*$res[10]["-6 months"]/$admincount)."%）
-*三個月：".$res[10]["-3 months"]."人（".round(100*$res[10]["-3 months"]/$admincount)."%）
-*一個月：".$res[10]["-1 month"]."人（".round(100*$res[10]["-1 month"]/$admincount)."%）
+===沒有作出管理方面行為的管理員===
+*一年：".$res[1]["-1 year"]."人（".round(100*$res[1]["-1 year"]/$admincount, 1)."%）
+*半年：".$res[1]["-6 months"]."人（".round(100*$res[1]["-6 months"]/$admincount, 1)."%）
+*三個月：".$res[1]["-3 months"]."人（".round(100*$res[1]["-3 months"]/$admincount, 1)."%）
+*一個月：".$res[1]["-1 month"]."人（".round(100*$res[1]["-1 month"]/$admincount, 1)."%）
 
-===管理方面編輯次數少於100次===
-*一年：".$res[100]["-1 year"]."人（".round(100*$res[100]["-1 year"]/$admincount)."%）
-*半年：".$res[100]["-6 months"]."人（".round(100*$res[100]["-6 months"]/$admincount)."%）
-*三個月：".$res[100]["-3 months"]."人（".round(100*$res[100]["-3 months"]/$admincount)."%）
+===管理方面行為次數少於10次===
+*一年：".$res[10]["-1 year"]."人（".round(100*$res[10]["-1 year"]/$admincount, 1)."%）
+*半年：".$res[10]["-6 months"]."人（".round(100*$res[10]["-6 months"]/$admincount, 1)."%）
+*三個月：".$res[10]["-3 months"]."人（".round(100*$res[10]["-3 months"]/$admincount, 1)."%）
+*一個月：".$res[10]["-1 month"]."人（".round(100*$res[10]["-1 month"]/$admincount, 1)."%）
 
-===管理方面編輯次數少於500次===
-*一年：".$res[500]["-1 year"]."人（".round(100*$res[500]["-1 year"]/$admincount)."%）
-*半年：".$res[500]["-6 months"]."人（".round(100*$res[500]["-6 months"]/$admincount)."%）
-*三個月：".$res[500]["-3 months"]."人（".round(100*$res[500]["-3 months"]/$admincount)."%）";
+===管理方面行為次數少於100次===
+*一年：".$res[100]["-1 year"]."人（".round(100*$res[100]["-1 year"]/$admincount, 1)."%）
+*半年：".$res[100]["-6 months"]."人（".round(100*$res[100]["-6 months"]/$admincount, 1)."%）
+*三個月：".$res[100]["-3 months"]."人（".round(100*$res[100]["-3 months"]/$admincount, 1)."%）
+
+===管理方面行為次數少於500次===
+*一年：".$res[500]["-1 year"]."人（".round(100*$res[500]["-1 year"]/$admincount, 1)."%）
+*半年：".$res[500]["-6 months"]."人（".round(100*$res[500]["-6 months"]/$admincount, 1)."%）
+*三個月：".$res[500]["-3 months"]."人（".round(100*$res[500]["-3 months"]/$admincount, 1)."%）";
 
 echo $out."\n";
 
