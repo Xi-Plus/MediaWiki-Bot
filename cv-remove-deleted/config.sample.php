8230b6f71e3dd31261c811b03076d86fd319c632
xiplus <huangxuanyuxiplus@gmail.com>
2018-01-16 19:40:14 +0800

    +cv-remove-deleted

---
+++ cv-remove-deleted/config.sample.php
@@ -0,0 +1,15 @@
+<?php
+
+$C["wikiapi"] = "https://zh.wikipedia.org/w/api.php";
+$C["user"] = "A2093064-bot";
+$C["pass"] = "";
+
+$C["page"] = "Wikipedia:頁面存廢討論/疑似侵權";
+
+$C["text1"] = "==當前的疑似侵權條目==";
+
+$C["summary_prefix"] = "移除已刪除頁面的提報";
+
+$C["User-Agent"] = "User:A2093064-bot cv-remove-deleted";
+
+$C["cookiefile"] = __DIR__."/../tmp/cv-remove-deleted-cookie.txt";
