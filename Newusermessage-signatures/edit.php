931e0d54b9a421add8b786a91d4864f5b37e6c23
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-03-11 19:13:17 +0800

    NUMS: new ns alias & show error message

--- Newusermessage-signatures/edit.php
+++ Newusermessage-signatures/edit.php
@@ -42,7 +42,7 @@ for ($i=0; $i < $C["fail_retry"]; $i++) {
 		}
 		$sign = $m[2][$key];
 		$user = "";
-		if (preg_match_all("/\[\[(?:User:|用户:|User talk:|User_talk:|用户讨论:|Special:Contributions\/|Special:用户贡献\/|Special:用戶貢獻\/|特殊:用户贡献\/|特殊:用戶貢獻\/)([^|\/]+)/i", $sign, $m2)) {
+		if (preg_match_all("/\[\[(?:U:|UT:|User:|用户:|User talk:|User_talk:|用户讨论:|Special:Contributions\/|Special:用户贡献\/|Special:用戶貢獻\/|特殊:用户贡献\/|特殊:用戶貢獻\/)([^|\/]+)/i", $sign, $m2)) {
 			$user = $m2[1][0];
 		}
 		$res = cURL($C["wikiapi"]."?".http_build_query(array(
@@ -57,6 +57,9 @@ for ($i=0; $i < $C["fail_retry"]; $i++) {
 			exit("fetch page fail\n");
 		}
 		$res = json_decode($res, true);
+		if (!isset($res["query"])) {
+			var_dump($res);
+		}
 		$time = strtotime($res["query"]["usercontribs"][0]["timestamp"]);
 		$date = date("Y年m月d日", $time)." (".$C["day"][date("w", $time)].") ".date("H:i", $time)." (UTC)";
 		$len = mb_strlen($sign);
