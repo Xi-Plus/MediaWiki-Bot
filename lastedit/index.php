81a581cbc4541806c5045237a742f32f4dac7ddc
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-03-27 18:28:58 +0800

    lastedit: show error msg

--- lastedit/index.php
+++ lastedit/index.php
@@ -6,8 +6,16 @@ $out = "";
 foreach ($list as $page) {
 	$res = file_get_contents("https://zh.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvlimit=1&rvdir=older&titles=".urlencode($page));
 	$res = json_decode($res, true);
+	if (!isset($res["query"])) {
+		var_dump($res);
+		continue;
+	}
 	$res = $res["query"]["pages"];
 	$res = current($res);
+	if (!isset($res["revisions"])) {
+		var_dump($res);
+		continue;
+	}
 	echo $res["revisions"][0]["timestamp"]." ".$page."\n";
 	$out .= $res["revisions"][0]["timestamp"]."\t".$page."\n";
 }
