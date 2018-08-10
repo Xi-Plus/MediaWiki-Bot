63eecf165945fd7e67986ce3f1c9e3a0976fdff0
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-08-10 17:19:41 +0800

    afd-section-title: automatic run for past some days

--- afd-section-title/edit.py
+++ afd-section-title/edit.py
@@ -6,16 +6,9 @@ import json
 import re
 import pywikibot
 import mwparserfromhell
+from datetime import datetime, timedelta
 from pywikibot.data.api import Request
 from config import *
-
-if len(sys.argv) >= 1:
-	pagename = sys.argv[1]
-else :
-	pagename = input("afd name:")
-
-if not pagename.startswith("Wikipedia:頁面存廢討論/記錄/"):
-	pagename = "Wikipedia:頁面存廢討論/記錄/" + pagename
 
 site = pywikibot.Site()
 site.login()
@@ -28,11 +21,6 @@ print(json.dumps(cfg, indent=4, ensure_ascii=False))
 if not cfg["enable"]:
 	exit("disabled\n")
 
-afdpage = pywikibot.Page(site, pagename)
-text = afdpage.text
-
-wikicode = mwparserfromhell.parse(text)
-
 def converttitle(title):
 	r = Request(site=site, parameters={
 		'action': 'query',
@@ -43,81 +31,105 @@ def converttitle(title):
 	data = r.submit()
 	return list(data['query']['pages'].values())[0]['title']
 
-for section in wikicode.get_sections()[1:]:
-	title = str(section.get(0).title)
-	print(title)
-	if re.search(r"{{\s*(delh|TalkendH)\s*\|", str(section), re.IGNORECASE) != None:
-		print("  closed, skip")
-		continue
+def fix(pagename):
+	if not pagename.startswith("Wikipedia:頁面存廢討論/記錄/"):
+		pagename = "Wikipedia:頁面存廢討論/記錄/" + pagename
 
-	m = re.search(r"^\[\[([^\]]+)\]\]$", title, re.IGNORECASE)
-	if m != None:
-		title = m.group(1)
-		start = ""
-		if title[0] == ":":
-			start = ":"
-			title = title[1:]
+	print("-"*50)
+	print("running for "+pagename)
 
-		title = converttitle(title)
+	afdpage = pywikibot.Page(site, pagename)
+	text = afdpage.text
 
-		title = "[["+start+title+"]]"
+	wikicode = mwparserfromhell.parse(text)
 
-		if str(section.get(0).title) != title:
-			if str(section.get(0).title).replace("_", " ") != title:
-				section.insert(1, "\n{{formerly|"+str(section.get(0).title)+"}}")
-			print("  set new title = "+title)
-			section.get(0).title = title
-		continue
-	
-	m = re.search(r"^(\[\[[^\]]+\]\][、，])+\[\[[^\]]+\]\]$", title, re.IGNORECASE)
-	if m != None:
-		titlelist = m.group(0).replace("]]，[[", "]]、[[").split("、")
-		newtitlelist = []
-		for title in titlelist:
-			if title.startswith("[[") and title.endswith("]]"):
-				title = title[2:-2]
+	for section in wikicode.get_sections()[1:]:
+		title = str(section.get(0).title)
+		print(title)
+		if re.search(r"{{\s*(delh|TalkendH)\s*\|", str(section), re.IGNORECASE) != None:
+			print("  closed, skip")
+			continue
 
+		m = re.search(r"^\[\[([^\]]+)\]\]$", title, re.IGNORECASE)
+		if m != None:
+			title = m.group(1)
+			start = ""
+			if title[0] == ":":
+				start = ":"
+				title = title[1:]
+
+			title = converttitle(title)
+
+			title = "[["+start+title+"]]"
+
+			if str(section.get(0).title) != title:
+				if str(section.get(0).title).replace("_", " ") != title:
+					section.insert(1, "\n{{formerly|"+str(section.get(0).title)+"}}")
+				print("  set new title = "+title)
+				section.get(0).title = title
+			continue
+		
+		m = re.search(r"^(\[\[[^\]]+\]\][、，])+\[\[[^\]]+\]\]$", title, re.IGNORECASE)
+		if m != None:
+			titlelist = m.group(0).replace("]]，[[", "]]、[[").split("、")
+			newtitlelist = []
+			for title in titlelist:
+				if title.startswith("[[") and title.endswith("]]"):
+					title = title[2:-2]
+
+					if title[0] == ":":
+						title = title[1:]
+
+					title = converttitle(title)
+
+					newtitlelist.append(title)
+				else :
+					print("  wrong title: "+title)
+					return
+			title = "{{al|"+"|".join(newtitlelist)+"}}"
+			if str(section.get(0).title) != title:
+				print("  set new title = "+title)
+				section.get(0).title = title
+			continue
+
+		m = re.search(r"^{{al\|([^\]]+\|)+[^\]]+}}$", title, re.IGNORECASE)
+		if m != None:
+			titlelist = m.group(0)[5:-2].split("|")
+			newtitlelist = []
+			for title in titlelist:
 				if title[0] == ":":
 					title = title[1:]
 
 				title = converttitle(title)
 
 				newtitlelist.append(title)
-			else :
-				exit("  wrong title: "+title)
-		title = "{{al|"+"|".join(newtitlelist)+"}}"
-		if str(section.get(0).title) != title:
-			print("  set new title = "+title)
-			section.get(0).title = title
-		continue
+			title = "{{al|"+"|".join(newtitlelist)+"}}"
+			if str(section.get(0).title) != title:
+				print("  set new title = "+title)
+				section.get(0).title = title
+			continue
 
-	m = re.search(r"^{{al\|([^\]]+\|)+[^\]]+}}$", title, re.IGNORECASE)
-	if m != None:
-		titlelist = m.group(0)[5:-2].split("|")
-		newtitlelist = []
-		for title in titlelist:
-			if title[0] == ":":
-				title = title[1:]
+		print("  unknown format, skip")
 
-			title = converttitle(title)
+	text = str(wikicode)
 
-			newtitlelist.append(title)
-		title = "{{al|"+"|".join(newtitlelist)+"}}"
-		if str(section.get(0).title) != title:
-			print("  set new title = "+title)
-			section.get(0).title = title
-		continue
+	if afdpage.text == text:
+		print("  nothing changed")
+		return
 
-	print("  unknown format, skip")
+	pywikibot.showDiff(afdpage.text, text)
+	summary = cfg["summary"]
+	print(summary)
+	afdpage.text = text
+	afdpage.save(summary=summary, minor=False)
 
-text = str(wikicode)
 
-if afdpage.text == text:
-	exit("  nothing changed")
-
-pywikibot.showDiff(afdpage.text, text)
-summary = cfg["summary"]
-print(summary)
-input("Save?")
-afdpage.text = text
-afdpage.save(summary=summary, minor=False)
+if len(sys.argv) >= 2:
+	pagename = sys.argv[1]
+	fix(pagename)
+else:
+	print("run past {} days".format(cfg["run_past_days"]))
+	for delta in range(cfg["run_past_days"]):
+		rundate = datetime.now() - timedelta(days=delta)
+		pagename = rundate.strftime('%Y/%m/%d')
+		fix(pagename)
