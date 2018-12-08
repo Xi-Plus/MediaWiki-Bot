77ef6863e6ecba3e2a245a2858d893ddcdb2f28e
xiplus <huangxuanyuxiplus@gmail.com>
2018-12-08 23:25:00 +0800

    remove-broken-file: Remove deleted file, skip checked

--- remove-broken-file/edit.py
+++ remove-broken-file/edit.py
@@ -10,6 +10,7 @@ os.environ['TZ'] = 'UTC'
 
 site = pywikibot.Site()
 site.login()
+sitecommons = pywikibot.Site("commons", "commons")
 
 config_page = pywikibot.Page(site, config_page_name)
 cfg = config_page.text
@@ -21,10 +22,20 @@ if not cfg["enable"]:
 
 cat = pywikibot.Page(site, "Category:缺少文件的条目")
 
+skippages = ""
+with open("skipedpage.txt", "r") as f:
+    skippages = f.read()
+skipfile = open("skipedpage.txt", "a")
+
 for page in site.categorymembers(cat):
-    print(page.title())
+    pagetitle = page.title()
+    print(pagetitle)
+    if pagetitle in skippages:
+        print("skip")
+        continue
     text = page.text
-    summary = []
+    summary_comment = []
+    summary_remove = []
     for image in page.imagelinks():
         if not image.exists():
             try:
@@ -34,39 +45,110 @@ for page in site.categorymembers(cat):
                 pass
             
             imagename = image.title(with_ns=False)
-            
+
             imageregex = "[" + imagename[0].upper() + imagename[0].lower() + "]" + re.escape(imagename[1:])
-            imageregex = imageregex.replace("\ ", "[ _]")
-            imageregex = imageregex
+            imageregex = imageregex.replace("\\ ", "[ _]")
             
-            existonen = pywikibot.Page(site, "en:File:{}".format(imagename)).exists()
+            checkotherwiki = {
+                "en": "英文維基百科"
+            }
+            existother = None
+            for wiki in checkotherwiki:
+                if pywikibot.Page(site, "{}:File:{}".format(wiki, imagename)).exists():
+                    existother = wiki
+                    break
+
+            deleted = False
             
-            if not existonen:
+            if existother is None:
                 print("File:{} not exist".format(imagename))
-            
-                regex = r"^(\s*\|\s*(?:image|logo|map_image)\s*=\s*(?:(File|Image):)?)"+imageregex+"\s*$"
-                text = re.sub(regex, r"\1", text, flags=re.M)
 
-                regex = r"\[\[(File|Image):" + imageregex + r"\s*\|[^\]]+\]\](?:\n(?!\s*\|))?"
-                text = re.sub(regex, r"", text)
+                data = pywikibot.data.api.Request(site=site, parameters={
+                    'action': 'query',
+                    'letitle': image.title(),
+                    "list": "logevents",
+                    "letype": "delete",
+                    "lelimit": "1"
+                    }).submit()
+                if len(data['query']['logevents']) > 0:
+                    deleted = True
+                    deletelog = data['query']['logevents'][0]
+                    if "CSD F7" in deletelog['comment']:
+                        deleted = False
+                deletedoncommons = False
+                if not deleted:
+                    data = pywikibot.data.api.Request(site=sitecommons, parameters={
+                        'action': 'query',
+                        'letitle': image.title(),
+                        "list": "logevents",
+                        "letype": "delete",
+                        "lelimit": "1"
+                        }).submit()
+                    if len(data['query']['logevents']) > 0:
+                        deleted = True
+                        deletedoncommons = True
+                        deletelog = data['query']['logevents'][0]
+                        print(deletelog)
                 
-                summary.append("移除不存在檔案 File:{}".format(imagename))
+                regex = r"(^\s*\|\s*(?:image|logo|map_image)\s*=\s*)((?:(?:File|Image):)?{0})\s*$".format(imageregex)
+                if deleted:
+                    replace = r"\1"
+                else:
+                    replace = r"\1<!-- 檔案不存在 \2  -->"
+
+                text = re.sub(regex, replace, text, flags=re.M)
+
+                regex = r"(\[\[(?:File|Image):{0}\s*(?:\|(?:\[\[[^[\]]*\]\]|[^[\]])*)?\]\])[ \t]*".format(imageregex)
+                if deleted:
+                    replace = ""
+                else:
+                    replace = r"<!-- 檔案不存在 \1 -->"
+
+                text = re.sub(regex, replace, text)
+
             else:
-                print("File:{} exist on en".format(imagename))
+                print("File:{} exist on {}".format(imagename, existother))
                 
-                regex = r"^(\s*\|\s*(?:image|logo|map_image)\s*=\s*)((?:File:)?"+imageregex+"\s*)$"
-                text = re.sub(regex, r"\1<!-- \2 ，可於英文維基百科取得 -->", text, flags=re.M)
+                regex = r"^(\s*\|\s*(?:image|logo|map_image)\s*=\s*)((?:(?:File|Image):)?{0}\s*)$".format(imageregex)
+                if deleted:
+                    replace = r"\1"
+                else:
+                    replace = r"\1<!-- 檔案不存在 \2 ，可從{0}取得 -->".format(checkotherwiki[existother])
+                text = re.sub(regex, replace, text, flags=re.M)
 
-                regex = r"(\[\[File:" + imageregex + r"\s*\|[^\]]+\]\])"
-                text = re.sub(regex, r"<!-- \1 ，可於英文維基百科取得 -->", text)
-                
-                summary.append("注釋掉檔案 File:{0} ，不存在於本地，可於[[en:File:{0}|英文維基百科]]取得".format(imagename))
+                regex = r"(\[\[File:{0}\s*(?:\|(?:\[\[[^[\]]*\]\]|[^[\]])*)?\]\])[ \t]*".format(imageregex)
+                if deleted:
+                    replace = ""
+                else:
+                    replace = r"<!-- 檔案不存在 \1 ，可從{0}取得 -->".format(checkotherwiki[existother])
+                text = re.sub(regex, replace, text)
+
+            if deleted:
+                if deletedoncommons:
+                    summary_remove.append("{0}，已被[[commons:User:{1}|{1}]][[commons:Special:Redirect/logid/{2}|刪除]]".format(imagename, deletelog["user"], deletelog["logid"]))
+                else:
+                    summary_remove.append("{0}，已被[[User:{1}|{1}]][[Special:Redirect/logid/{2}|刪除]]：{3}".format(imagename, deletelog["user"], deletelog["logid"], deletelog["comment"]))
+            else:
+                if existother:
+                    summary_comment.append("{0}（[[{1}:File:{0}|從{1}取得]]）".format(imagename, existother))
+                else:
+                    summary_comment.append(imagename)
+
     if page.text == text:
         print("nothing changed")
+        skipfile.write(pagetitle + "\n")
         continue
+
     pywikibot.showDiff(page.text, text)
+
+    summary = []
+    if len(summary_comment):
+        summary.append("注釋不存在檔案：" + "、".join(summary_comment))
+    if len(summary_remove):
+        summary.append("移除檔案：" + "、".join(summary_remove))
     summary = "；".join(summary)
     print("summary = {}".format(summary))
+
     save = input("save?")
     if save in ["Yes", "yes", "Y", "y"]:
         page.text = text
