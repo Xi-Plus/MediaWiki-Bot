4803b62efcc9cd0182ff5ad86422cb4d4ede4171
xiplus <huangxuanyuxiplus@gmail.com>
2019-01-24 09:51:38 +0800

    Create function\getPageContent

---
+++ function/getPageContent.php
@@ -0,0 +1,18 @@
+<?php
+require_once(__DIR__.'/curl.php');
+
+function getPageContent($api, $title) {
+	$res = cURL($api."?".http_build_query(array(
+		"action" => "query",
+		"prop" => "revisions",
+		"format" => "json",
+		"rvprop" => "content|timestamp",
+		"titles" => $title
+	)));
+	if ($res === false) {
+		return false;
+	}
+	$res = json_decode($res, true);
+	$pages = current($res["query"]["pages"]);
+	return $pages["revisions"][0];
+}
