b67906401f18f2443e69e5dcc41a92caf64620f7
xiplus <huangxuanyuxiplus@gmail.com>
2019-02-10 09:50:37 +0800

    afd-section-title: Check vfd template

--- afd-section-title/edit.py
+++ afd-section-title/edit.py
@@ -27,6 +27,7 @@ if not cfg["enable"]:
 
 
 def converttitle(title):
+    oldtitle = title
     r = Request(site=site, parameters={
         'action': 'query',
         'titles': title,
@@ -42,20 +43,37 @@ def converttitle(title):
         mode.append('converted')
     if 'normalized' in data['query']:  # 命名空間
         mode.append('normalized')
+    if 'redirects' not in mode:
+        page = pywikibot.Page(site, title)
+        if page.exists() and re.search(r'{{\s*[vaictumr]fd', page.text):
+            mode.append('vfd_on_source')
+    else:
+        page = pywikibot.Page(site, oldtitle)
+        if page.exists() and re.search(r'{{\s*[vaictumr]fd', page.text):
+            mode.append('vfd_on_source')
+        page = pywikibot.Page(site, title)
+        if page.exists() and re.search(r'{{\s*[vaictumr]fd', page.text):
+            mode.append('vfd_on_target')
+    if not 'vfd_on_source' in mode and not 'vfd_on_target' in mode:
+        mode.append('no_vfd')
     return {'title': title, 'mode': mode}
 
 
 def appendComment(text, mode):
     text = text.strip()
-    comment = []
-    if 'redirects' in mode and isinstance(cfg['comment_redirects'], str):
-        comment.append(cfg['comment_redirects'])
-    if 'converted' in mode and isinstance(cfg['comment_converted'], str):
-        comment.append(cfg['comment_converted'])
-    if 'normalized' in mode and isinstance(cfg['comment_normalized'], str):
-        comment.append(cfg['comment_normalized'])
-    if len(comment) > 0:
-        text += '\n' + cfg['comment'].format(''.join(comment)) + '\n\n'
+    if 'fix' in mode:
+        comment = []
+        if 'redirects' in mode and isinstance(cfg['comment_fix']['redirects'], str):
+            comment.append(cfg['comment_fix']['redirects'])
+        if 'converted' in mode and isinstance(cfg['comment_fix']['converted'], str):
+            comment.append(cfg['comment_fix']['converted'])
+        if 'normalized' in mode and isinstance(cfg['comment_fix']['normalized'], str):
+            comment.append(cfg['comment_fix']['normalized'])
+        if len(comment) > 0:
+            text += '\n' + cfg['comment_fix']['main'].format(''.join(comment))
+    if 'no_vfd' in mode:
+        text += '\n' + cfg['comment_vfd']
+    text += '\n\n'
     return text
 
 
@@ -71,9 +89,12 @@ def fix(pagename):
 
     wikicode = mwparserfromhell.parse(text)
 
-    for section in wikicode.get_sections()[1:]:
+    changes = []
+    for secid, section in enumerate(list(wikicode.get_sections())):
+        if secid == 0:
+            continue
         title = str(section.get(0).title)
-        print(title)
+        print(secid, title)
         if re.search(r"{{\s*(delh|TalkendH)\s*\|", str(section), re.IGNORECASE) != None:
             print("  closed, skip")
             continue
@@ -89,8 +110,12 @@ def fix(pagename):
             mode = []
 
             convert = converttitle(title)
-            title = convert['title']
+            if (('redirects' in convert['mode'] and 'vfd_on_target' in convert['mode'])
+                    or (not 'redirects' in convert['mode'])):
+                title = convert['title']
+                mode.append('fix')
             mode += convert['mode']
+            print('    ', mode)
 
             title = "[["+start+title+"]]"
 
@@ -98,10 +123,11 @@ def fix(pagename):
                 if str(section.get(0).title).replace("_", " ") != title:
                     section.insert(
                         1, "\n{{formerly|"+str(section.get(0).title)+"}}")
+                    pass
                 print("  set new title = "+title)
                 section.get(0).title = title
-                newtext = appendComment(str(section), mode)
-                section.replace(section, newtext)
+            newtext = appendComment(str(section), mode)
+            changes.append([secid, newtext])
             continue
 
         m = re.search(
@@ -118,7 +144,10 @@ def fix(pagename):
                         title = title[1:]
 
                     convert = converttitle(title)
-                    title = convert['title']
+                    if (('redirects' in convert['mode'] and 'vfd_on_target' in convert['mode'])
+                            or (not 'redirects' in convert['mode'])):
+                        title = convert['title']
+                        mode.append('fix')
                     mode += convert['mode']
 
                     newtitlelist.append(title)
@@ -129,8 +158,8 @@ def fix(pagename):
             if str(section.get(0).title) != title:
                 print("  set new title = "+title)
                 section.get(0).title = title
-                newtext = appendComment(str(section), mode)
-                section.replace(section, newtext)
+            newtext = appendComment(str(section), mode)
+            changes.append([section, newtext])
             continue
 
         m = re.search(r"^{{al\|([^\]]+\|)+[^\]]+}}$", title, re.IGNORECASE)
@@ -143,7 +172,10 @@ def fix(pagename):
                     title = title[1:]
 
                 convert = converttitle(title)
-                title = convert['title']
+                if (('redirects' in convert['mode'] and 'vfd_on_target' in convert['mode'])
+                        or (not 'redirects' in convert['mode'])):
+                    title = convert['title']
+                    mode.append('fix')
                 mode += convert['mode']
 
                 newtitlelist.append(title)
@@ -151,13 +183,18 @@ def fix(pagename):
             if str(section.get(0).title) != title:
                 print("  set new title = "+title)
                 section.get(0).title = title
-                newtext = appendComment(str(section), mode)
-                section.replace(section, newtext)
+            newtext = appendComment(str(section), mode)
+            section.replace(section, newtext)
             continue
 
         print("  unknown format, skip")
 
-    text = str(wikicode)
+    for change in changes:
+        wikicode = mwparserfromhell.parse(text)
+        sections = list(wikicode.get_sections())
+        secid, newtext = change
+        wikicode.replace(sections[secid], newtext)
+        text = str(wikicode)
 
     if afdpage.text == text:
         print("  nothing changed")
