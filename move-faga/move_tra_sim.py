73bee3ab742216ba58383e0de4f0d946c32039c4
xiplus <huangxuanyuxiplus@gmail.com>
2019-02-10 20:46:59 +0800

    move-faga/move_tra_sim: Skip redirect and target exist

--- move-faga/move_tra_sim.py
+++ move-faga/move_tra_sim.py
@@ -21,10 +21,17 @@ with open('list.csv', 'r') as f:
             print('{} not found'.format(oldtitle))
             continue
 
+        if page.isRedirectPage():
+            print('{} is redirect'.format(oldtitle))
+            continue
+
         newtitle = oldtitle.replace('Wikipedia:優良條目', 'Wikipedia:优良条目')
         print('move', oldtitle, 'to', newtitle)
 
-        page.move(newtitle, reason=summary, movetalk=True, noredirect=True)
+        try:
+            page.move(newtitle, reason=summary, movetalk=True, noredirect=True)
+        except pywikibot.exceptions.ArticleExistsConflict as e:
+            print('{} target exist'.format(oldtitle))
 
         for backlink in page.embeddedin():
             if re.search(r'^Wikipedia:优良条目/\d+年\d+月\d+日$', backlink.title()):
@@ -33,7 +40,8 @@ with open('list.csv', 'r') as f:
                 if re.search(r'Wikipedia:優良條目/s', text):
                     print('skip')
                     continue
-                text = re.sub(r'{{(Wikipedia|维基百科):優良條目/', '{{Wikipedia:优良条目/', text)
+                text = re.sub(r'{{(Wikipedia|维基百科):優良條目/',
+                              '{{Wikipedia:优良条目/', text)
 
                 pywikibot.showDiff(backlink.text, text)
 
