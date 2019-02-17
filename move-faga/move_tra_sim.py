f6b55760d2da3e765015c74925f9c0ad46c0dde4
xiplus <huangxuanyuxiplus@gmail.com>
2019-02-17 20:57:05 +0800

    move-faga/move_tra_sim: Add limit

--- move-faga/move_tra_sim.py
+++ move-faga/move_tra_sim.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
+import argparse
 import os
 import re
 
+os.environ['PYWIKIBOT2_DIR'] = os.path.dirname(os.path.realpath(__file__))
 import pywikibot
 
 
-os.environ['PYWIKIBOT2_DIR'] = os.path.dirname(os.path.realpath(__file__))
 os.environ['TZ'] = 'UTC'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--limit', type=int, default=0)
+parser.add_argument('--limit2', type=int, default=0)
+args = parser.parse_args()
+print(args)
 
 site = pywikibot.Site()
 site.login()
@@ -14,6 +21,8 @@ site.login()
 summary = '機器人：[[Special:PermaLink/53078964#優良條目和典范条目子頁面繁簡皆有的問題|統一子頁面繁簡]]'
 
 with open('list.csv', 'r') as f:
+    cnt = 1
+    cnt2 = 1
     for title in f:
         oldtitle = title.strip()
         page = pywikibot.Page(site, oldtitle)
@@ -26,16 +35,20 @@ with open('list.csv', 'r') as f:
             continue
 
         newtitle = oldtitle.replace('Wikipedia:優良條目', 'Wikipedia:优良条目')
-        print('move', oldtitle, 'to', newtitle)
+        print('{} {} Moving from {} to {}'.format(
+            cnt, cnt2, oldtitle, newtitle))
 
         try:
             page.move(newtitle, reason=summary, movetalk=True, noredirect=True)
         except pywikibot.exceptions.ArticleExistsConflict as e:
             print('{} target exist'.format(oldtitle))
+            continue
+        cnt += 1
+        cnt2 += 1
 
         for backlink in page.embeddedin():
             if re.search(r'^Wikipedia:优良条目/\d+年\d+月\d+日$', backlink.title()):
-                print(backlink.title())
+                print('{} Editing {}'.format(cnt2, backlink.title()))
                 text = backlink.text
                 if re.search(r'Wikipedia:優良條目/s', text):
                     print('skip')
@@ -48,3 +61,12 @@ with open('list.csv', 'r') as f:
                 backlink.text = text
                 print(summary)
                 backlink.save(summary=summary, minor=True)
+                cnt2 += 1
+
+        if args.limit > 0 and cnt > args.limit:
+            print('Reach the limit. Quitting.')
+            break
+
+        if args.limit2 > 0 and cnt2 > args.limit2:
+            print('Reach the limit2. Quitting.')
+            break
