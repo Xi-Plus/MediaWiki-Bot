c9ce83e4634cfc13e555818ddf9ba87481328d23
xiplus <huangxuanyuxiplus@gmail.com>
2019-02-28 22:32:56 +0800

    template-use: Add url config

--- template-use/index.php
+++ template-use/index.php
@@ -1,9 +1,15 @@
 <?php
+require("config.php");
+
 $list = file_get_contents("input.txt");
 $list = str_replace("\r", "", $list);
 $list = explode("\n", $list);
+$output = "";
 foreach ($list as $page) {
-	$res = file_get_contents("https://xiplus.twbbs.org/Xiplus-zhWP/Template-transclusion-count.php?namespace=10&title=" . urlencode($page));
+	$res = file_get_contents(sprintf($C["url"], urlencode($page)));
 	$res = json_decode($res, true);
-	echo $res["result"] . " " . $page . "\n";
+	$text = $res["result"] . " " . $page . "\n";
+	echo $text;
+	$output .= $text;
 }
+file_put_contents("output.txt", $output);
