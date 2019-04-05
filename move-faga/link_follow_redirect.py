49b01aab8e810d9d84b70ea5bebf914ccf77672d
Xi-Plus <huangxuanyuxiplus@gmail.com>
2019-04-05 13:52:17 +0800

    Create link_follow_redirect.py

---
+++ move-faga/link_follow_redirect.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import argparse
+import os
+import re
+
+os.environ['PYWIKIBOT2_DIR'] = os.path.dirname(os.path.realpath(__file__))
+import pywikibot
+from pywikibot.data.api import Request
+
+
+def converttitle(title):
+    r = Request(site=site, parameters={
+        'action': 'query',
+        'titles': title,
+        'redirects': 1,
+        'converttitles': 1
+    })
+    data = r.submit()
+    try:
+        return list(data['query']['pages'].values())[0]['title']
+    except KeyError:
+        print(title, data)
+        return title
+
+
+os.environ['TZ'] = 'UTC'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('page')
+args = parser.parse_args()
+print(args)
+
+site = pywikibot.Site()
+site.login()
+
+listpage = pywikibot.Page(site, args.page)
+
+if not listpage.exists():
+    exit('not exists')
+
+listtext = listpage.text
+
+links = re.findall(r'\[\[([^\]|]+)(?:\]\]|\|)', listtext)
+print(links)
+
+for link in links:
+    if link.startswith('#'):
+        continue
+
+    newlink = converttitle(link)
+    if link != newlink:
+        listtext = re.sub(r'\[\[{}(\]\]|\|)'.format(
+            re.escape(link)), r'[[{}\1'.format(newlink), listtext)
+        print(link, newlink)
+
+with open('out.txt', 'w') as f:
+    f.write(listtext)
