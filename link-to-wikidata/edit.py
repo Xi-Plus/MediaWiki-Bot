7a0774169604576a376684e1b53ad85431e4198e
Xi-Plus <huangxuanyuxiplus@gmail.com>
2019-05-22 09:21:46 +0800

    Create link-to-wikidata

---
+++ link-to-wikidata/edit.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import csv
+import os
+import re
+
+os.environ['PYWIKIBOT_DIR'] = os.path.dirname(os.path.realpath(__file__))
+import pywikibot
+from config import *
+
+
+site = pywikibot.Site()
+site.login()
+
+with open('list.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        fromTitle = row[0]
+        toTitle = row[1]
+        print('Import from {} to {}'.format(fromTitle, toTitle))
+
+        inBlack = False
+        for blacklist in blacklists:
+            if re.search(blacklist, toTitle):
+                inBlack = True
+                break
+        if inBlack:
+            print('Skip')
+            continue
+
+        page = pywikibot.Page(site, fromTitle)
+        item = pywikibot.ItemPage.fromPage(page)
+
+        item.setSitelink(sitelink={'site': targetSite, 'title': toTitle}, summary='Add {}'.format(targetSite))
