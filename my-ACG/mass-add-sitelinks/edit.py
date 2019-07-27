366b62358b28183243eb63a2ecae958d90dea90f
xiplus <huangxuanyuxiplus@gmail.com>
2019-07-27 20:37:11 +0800

    Create mass-add-sitelinks

---
+++ my-ACG/mass-add-sitelinks/edit.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import argparse
+import csv
+import os
+
+os.environ['PYWIKIBOT_DIR'] = os.path.dirname(os.path.realpath(__file__))
+import pywikibot
+
+
+site = pywikibot.Site()
+site.login()
+datasite = site.data_repository()
+
+
+def addSitelinks(title, site, targettitle):
+    print(title)
+
+    if title[0] == 'Q':
+        myitem = pywikibot.ItemPage(datasite, title)
+    else:
+        print('\t Not Item page')
+        return
+
+    data = {
+        'site': site,
+        'title': targettitle
+    }
+    print('\t', data)
+    myitem.setSitelink(data, summary='自動設定wikidata連結')
+
+
+def main(filename):
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            addSitelinks(row[0], 'wikidatawiki', row[1])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', nargs='?')
+    args = parser.parse_args()
+    main(args.filename)
