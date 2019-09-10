34b2dd1aaf165ab683848fcc96aa6385cef580aa
xiplus <huangxuanyuxiplus@gmail.com>
2019-09-10 22:14:05 +0800

    Fix voting_info

--- RFA-stats/function.php
+++ RFA-stats/function.php
@@ -9,7 +9,7 @@ function voting_info($support, $oppose) {
 		}
 	} else {
 		if ($support >= $oppose * 4) {
-			return "要獲選最多還能有" . (floor($support / 4) - $oppose + 1) . "張反對票";
+			return "要獲選最多還能有" . (floor($support / 4) - $oppose) . "張反對票";
 		} else {
 			return "要獲選還需" . ($oppose * 4 - $support) . "張支持票";
 		}
