564ef5595ea3ea30da425abbf4a5e525edd7b3ed
xiplus <huangxuanyuxiplus@gmail.com>
2019-10-03 09:32:37 +0800

    Set rating

--- my-ACG/util/ani_gamer_com_tw_animeVideo.py
+++ my-ACG/util/ani_gamer_com_tw_animeVideo.py
@@ -1,8 +1,18 @@
+import argparse
+import re
 import requests
 from bs4 import BeautifulSoup
 
 
 class AniGamerComTwAnimeVideo:
+    RATING_IMG = {
+        'ALL': 0,
+        '6TO12': 6,
+        '12TO18': 12,
+        '15TO18': 15,
+        '18UP': 18,
+    }
+
     def getData(self, url):
         text = requests.get(url).text
         soup = BeautifulSoup(text, 'html.parser')
@@ -14,4 +24,18 @@ class AniGamerComTwAnimeVideo:
         else:
             data['episodes'] = len(season.find('ul').findAll('li'))
 
+        rating = soup.find('div', {'class': 'rating'})
+        if rating:
+            src = rating.find('img').get('src')
+            m = re.search(r'TW-(.+?)\.gif', src)
+            if m:
+                data['rating'] = self.RATING_IMG[m.group(1)]
+
         return data
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url')
+    args = parser.parse_args()
+    print(AniGamerComTwAnimeVideo().getData(args.url))
