af018d7f2754455d993bb56931e2c9c70abd6344
xiplus <huangxuanyuxiplus@gmail.com>
2019-11-18 19:11:54 +0800

    encoding=utf8

--- push/python/func.py
+++ push/python/func.py
@@ -6,6 +6,6 @@ def file_get_contents(filename):
     if filename.startswith('http'):
         res = urllib.request.urlopen(filename).read().decode()
     else:
-        with open(filename, 'r') as f:
+        with open(filename, 'r', encoding='utf8') as f:
             res = f.read()
     return res
