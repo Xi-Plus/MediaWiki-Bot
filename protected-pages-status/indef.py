a1556649df511299980665a0b0f89c03e74c391a
xiplus <huangxuanyuxiplus@gmail.com>
2019-12-14 10:31:09 +0800

    Update protected-pages-status

--- protected-pages-status/indef.py
+++ protected-pages-status/indef.py
@@ -20,42 +20,36 @@ if not cfg['longtime_enable']:
 
 conn = toolforge.connect('zhwiki')
 
-d = datetime.today() + timedelta(days=30)
-
 
 with conn.cursor() as cur:
+    cur.execute('use zhwiki_p')
     cur.execute("""
-        SELECT pr_page, page_namespace, page_title, pr_expiry
+        SELECT pr_page, page_namespace, page_title
         FROM page_restrictions
         LEFT JOIN page ON pr_page = page_id
         WHERE pr_type = "edit"
             AND pr_level = "autoconfirmed"
-            AND pr_expiry != "infinity"
-            AND pr_expiry >= {}
-            AND page_namespace IN (0, 4)
-        ORDER BY pr_expiry DESC, page_title ASC
-
-    """.format(d.strftime('%Y%m%d000000')))
+            AND pr_expiry = "infinity"
+            AND page_namespace = 0
+        ORDER BY page_title ASC
+    """)
     res = cur.fetchall()
 
 
-text = cfg['longtime_header_text'].format(d.strftime('%-m月%-d日'))
-
-text += """{{| class="wikitable sortable"
-!頁面!!保護日誌!!期限"""
+text = cfg['indef_header_text']
+text += """{| class="wikitable sortable"
+!頁面!!保護日誌"""
 
 for row in res:
     pid = str(row[0])
     ns = str(row[1])
     title = row[2].decode()
-    expiry = str(row[3].decode())
     text += "\n|-\n"
     text += "|[[Special:Redirect/page/" + pid + "|{{subst:#ifeq:" + ns + "|0||{{subst:ns:" + ns + "}}:}}" + title + "]]"
     text += "||[{{fullurl:Special:日志/protect|page={{subst:#ifeq:" + ns + "|0||{{subst:ns:" + ns + "}}:}}" + title + "}} 保護日誌]"
-    text += "||" + expiry
 text += "\n|}"
 
 
-page = pywikibot.Page(site, cfg['longtime_output_page'])
+page = pywikibot.Page(site, cfg['indef_output_page'])
 page.text = text
-page.save(summary=cfg['longtime_summary'], minor=False)
+page.save(summary=cfg['indef_summary'], minor=False)
