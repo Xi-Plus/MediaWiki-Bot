a04b4cbc7ca45b81fd12b18befcd39959a9313d4
xiplus <huangxuanyuxiplus@gmail.com>
2019-12-20 23:25:11 +0800

    Sort headers

--- not-report-np-pages/edit.py
+++ not-report-np-pages/edit.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 os.environ['PYWIKIBOT_DIR'] = os.path.dirname(os.path.realpath(__file__))
 import pywikibot
@@ -18,8 +19,6 @@ print(json.dumps(cfg, indent=4, ensure_ascii=False))
 if not cfg['enable']:
     exit('disabled\n')
 
-text = cfg['header_text']
-
 reported_pages = set()
 for page in pywikibot.Page(site, cfg['np_page']).linkedPages(namespaces=[0]):
     reported_pages.add(page.title())
@@ -31,19 +30,32 @@ for page in pywikibot.Category(site, cfg['afd_category']).members(namespaces=[0]
 # print(afd_pages)
 
 othertext = ''
+text_dict = {}
 for cate in pywikibot.Category(site, cfg['np_category']).members():
     if cate.namespace().id != 14:
         othertext += '# [[:{}]]\n'.format(cate.title())
         continue
-    text += '=== [[:{}]] ===\n'.format(cate.title())
+    m = re.search(r'^Category:自(\d+)年(\d+)月主題關注度不足的條目$', cate.title())
+    key = None
+    if m:
+        key = int(m.group(1)) * 100 + int(m.group(2))
+        text_dict[key] = '=== [[:{}]] ===\n'.format(cate.title())
     for page in cate.members():
         title = page.title()
         if title not in reported_pages and title not in afd_pages:
-            text += '# [[:{}]]\n'.format(title)
-    text += '\n'
+            if key:
+                text_dict[key] += '# [[:{}]]\n'.format(title)
+            else:
+                othertext += '# [[:{}]]\n'.format(title)
+
+text = cfg['header_text']
 
 if othertext != '':
     text += '=== 未知日期 ===\n' + othertext
+
+text_dict = sorted(text_dict.items())
+for _, temp_text in text_dict:
+    text += temp_text + '\n'
 
 print(text)
 
