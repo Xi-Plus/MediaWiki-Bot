3cf54b6ccba0e1fe6fd1e7ba9635b886bcc605c2
xiplus <huangxuanyuxiplus@gmail.com>
2020-08-25 10:55:37 +0800

    Update config.sample.php

--- remove-inuse/config.sample.php
+++ remove-inuse/config.sample.php
@@ -1,7 +1,7 @@
 <?php
 
 $C["wikiapi"] = "https://zh.wikipedia.org/w/api.php";
-$C["config_page_notice"] = "https://zh.wikipedia.org/w/index.php?title=User:A2093064-bot/task/6/config.json&action=raw";
+$C["config_page"] = "https://zh.wikipedia.org/w/index.php?title=User:A2093064-bot/task/6/config.json&action=raw";
 
 $C["user"] = "";
 $C["pass"] = "";
