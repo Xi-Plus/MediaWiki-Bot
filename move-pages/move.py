46ce27063bef185e7466fa6373f1b4c8e59872a5
xiplus <huangxuanyuxiplus@gmail.com>
2020-11-29 19:48:39 +0800

    Update move.py

--- move-pages/move.py
+++ move-pages/move.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
+import csv
 import os
-import pywikibot
 
 os.environ['PYWIKIBOT_DIR'] = os.path.dirname(os.path.realpath(__file__))
-os.environ['TZ'] = 'UTC'
+import pywikibot
 
 site = pywikibot.Site()
 site.login()
 
-with open("list.csv", "r") as f:
-    for title in f:
-        oldtitle = title.strip()
+with open('list.csv', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if len(row) < 3:
+            continue
+        oldtitle = row[0].strip()
+        newtitle = row[1].strip()
+        reason = row[2]
         page = pywikibot.Page(site, oldtitle)
-        ns = page.namespace().custom_name
-        title = page.titleWithoutNamespace()
-        title = title[0].upper() + title[1:]
-        newtitle = ns + ":" + title
-        print("move", oldtitle, "to", newtitle)
-        page.move(newtitle, reason="[[:phab:T187783]]，移動到大寫開頭")
+        print('move', oldtitle, 'to', newtitle, 'reason', reason)
+        page.move(newtitle, reason=row[2], noredirect=True)
