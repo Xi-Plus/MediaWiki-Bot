2e584cc90e274bec5fc265611eedb820837a9c05
xiplus <huangxuanyuxiplus@gmail.com>
2021-03-27 13:53:55 +0800

    Fix status handing

--- rfcuham-archive/rfcuham.py
+++ rfcuham-archive/rfcuham.py
@@ -33,32 +33,31 @@ count = 0
 for section in wikicode.get_sections()[2:]:
     title = str(section.get(0).title)
     print(title, end="\t")
+    status = '(empty)'
     for template in section.filter_templates():
-        if template.name.lower() == "Status":
+        template_name = template.name.lower().strip()
+        if template_name == 'status':
             if template.has(1):
                 status = template.get(1)
-            else:
-                status = "(empty)"
-            print("status", status, end="\t")
-            if status in cfg["status_to_archive"]:
-                lasttime = datetime(1, 1, 1)
-                for m in re.findall(r"(\d{4})年(\d{1,2})月(\d{1,2})日 \(.\) (\d{2}):(\d{2}) \(UTC\)", str(section)):
-                    d = datetime(int(m[0]), int(m[1]), int(m[2]), int(m[3]), int(m[4]))
-                    lasttime = max(lasttime, d)
-                print(lasttime, end="\t")
-                if time.time() - lasttime.timestamp() > cfg["time_to_live"] and lasttime != datetime(1, 1, 1):
-                    target = (lasttime.year, lasttime.month)
-                    if target not in archivelist:
-                        archivelist[target] = []
-                    archivestr = str(section).strip()
-                    archivestr = re.sub(
-                        r"{{bot-directive-archiver\|no-archive-begin}}[\s\S]+?{{bot-directive-archiver\|no-archive-end}}\n?", "", archivestr)
-                    archivelist[target].append(archivestr)
-                    count += 1
-                    section.remove(section)
-                    print("archive", end="\t")
-                break
             break
+    print('status', status, end='\t')
+    if status in cfg['status_to_archive']:
+        lasttime = datetime(1, 1, 1)
+        for m in re.findall(r'(\d{4})年(\d{1,2})月(\d{1,2})日 \(.\) (\d{2}):(\d{2}) \(UTC\)', str(section)):
+            d = datetime(int(m[0]), int(m[1]), int(m[2]), int(m[3]), int(m[4]))
+            lasttime = max(lasttime, d)
+        print(lasttime, end='\t')
+        if time.time() - lasttime.timestamp() > cfg['time_to_live'] and lasttime != datetime(1, 1, 1):
+            target = (lasttime.year, lasttime.month)
+            if target not in archivelist:
+                archivelist[target] = []
+            archivestr = str(section).strip()
+            archivestr = re.sub(
+                r'{{bot-directive-archiver\|no-archive-begin}}[\s\S]+?{{bot-directive-archiver\|no-archive-end}}\n?', '', archivestr)
+            archivelist[target].append(archivestr)
+            count += 1
+            section.remove(section)
+            print('archive', end='\t')
     print()
 
 text = str(wikicode)
