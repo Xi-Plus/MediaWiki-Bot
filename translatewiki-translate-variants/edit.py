71375cf868276c627e0c93dfde926ea0139ce05d
xiplus <huangxuanyuxiplus@gmail.com>
2021-05-17 09:24:21 +0800

    Create translatewiki-translate-variants

---
+++ translatewiki-translate-variants/edit.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+import html
+import json
+import os
+import re
+import time
+
+import requests
+
+os.environ['PYWIKIBOT_DIR'] = os.path.dirname(os.path.realpath(__file__))
+import pywikibot
+
+from util import headers, noteTA
+
+
+def translateText(strings, tolang, noteTA, headers):
+    def escapeWikitextMatch(text):
+        return '&#{};'.format(ord(text[0]))
+
+    def escapeWikitext(text):
+        text = re.sub(r"[\[\]{}<>|\\:*'_#&\s]", escapeWikitextMatch, text)
+        return text
+
+    text = noteTA
+    for idx, string in enumerate(strings):
+        text += '<div id="text{}">{}</div>'.format(idx, escapeWikitext(string))
+
+    data = {
+        'action': 'parse',
+        'format': 'json',
+        'text': text,
+        'prop': 'text',
+        'contentmodel': 'wikitext',
+        'utf8': 1,
+        'uselang': tolang
+    }
+    r = requests.post('https://zh.wikipedia.org/w/api.php', data=data, headers=headers)
+    try:
+        result = r.json()
+    except Exception as e:
+        print(e)
+        print(r.text)
+        raise e
+
+    newstrings = [''] * len(strings)
+
+    result = result['parse']['text']['*']
+    matches = re.findall(r'<div id="text(\d+)">(.+?)</div>', result)
+    for match in matches:
+        idx = int(match[0])
+        newtext = html.unescape(match[1]).replace('\\n', '\\\\n')
+        newstrings[idx] = newtext
+
+    return newstrings
+
+
+site = pywikibot.Site()
+site.login()
+
+with open('list.txt', 'r', encoding='utf8') as f:
+    for key in f:
+        key = key.strip()
+
+        namehant = '{}/zh-hant'.format(key)
+        namehans = '{}/zh-hans'.format(key)
+
+        pagehant = pywikibot.Page(site, namehant)
+
+        if not pagehant.exists():
+            print('{} is not exists'.format(namehant))
+            continue
+
+        newtext = translateText([pagehant.text], 'zh-cn', noteTA, headers)[0]
+
+        pagehans = pywikibot.Page(site, namehans)
+        summary = '自動從[[{}]]轉換'.format(namehant)
+        print('Editing {} with summary {}'.format(namehans, summary))
+        print(pagehant.text)
+        print('-' * 50)
+        pywikibot.showDiff(pagehans.text, newtext)
+        input('Save?')
+        pagehans.text = newtext
+
+        pagehans.save(summary=summary, asynchronous=True)
---
+++ translatewiki-translate-variants/util.py
@@ -0,0 +1,43 @@
+noteTA = '''{{NoteTA
+|G1=IT
+|G2=MediaWiki
+}}
+-{H|zh-hans:帮助;zh-hant:說明}-
+-{H|zh-hans:周;zh-hant:週}-
+-{H|zh-hans:批复;zh-hant:批復}-
+-{H|zh-hans:配置;zh-hant:配置}-
+-{H|zh-hans:窗口;zh-hant:視窗}-
+-{H|zh-hans:项目;zh-hant:項目}-
+-{H|zh-hans:单击;zh-hant:點擊}-
+-{H|zh-hans:支持;zh-hant:支援}-
+-{H|zh-hans:标清;zh-hant:標清}-
+-{H|zh-hans:移动设备;zh-hant:行動裝置}-
+-{H|zh-hans:关联;zh-hant:關聯}-
+-{H|zh-hans:保存;zh-hant:儲存}-
+-{H|zh-hans:执行;zh-hant:執行}-
+-{H|zh-hans:消息;zh-hant:訊息}-
+-{H|zh-hans:启动;zh-hant:啟動}-
+-{H|zh-hans:启用功能;zh-hant:啟用功能}-
+-{H|zh-hans:启用;zh-hant:啟用}-
+-{H|zh-hans:计划;zh-hant:計劃}-
+-{H|zh-hans:不实资料;zh-hant:不實資料}-
+-{H|zh-hans:注释;zh-hant:注釋}-
+-{H|zh-hans:分辨率;zh-hant:解析度}-
+-{H|zh-hans:类型;zh-hant:類別}-
+-{H|zh-hans:账户;zh-hant:帳號}-
+-{H|zh-hans:已在运行;zh-hant:已在執行}-
+-{H|zh-hans:当前;zh-hant:目前}-
+-{H|zh-hans:最近更改;zh-hant:近期變更}-
+-{H|zh-hans:相关更改;zh-hant:相關變更}-
+-{H|zh-hans:说明;zh-hant:說明}-
+-{H|zh-hans:Twinkle帮助;zh-hant:Twinkle說明}-
+-{H|zh-hans:公用IP;zh-hant:公共IP}-
+-{H|zh-hans:监视;zh-hant:監視}-
+-{H|zh-hans:通过;zh-hant:透過}-
+-{H|zh-hans:链入;zh-hant:連入}-
+-{H|zh-hans:消链;zh-hant:消連}-
+'''
+
+headers = {
+    'user-agent': 'Twinkle i18n script [[User:Xiplus]]'
+}
