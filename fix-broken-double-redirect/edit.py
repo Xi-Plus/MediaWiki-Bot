536e6372221508bc34f721aa78a36d5023505abb
xiplus <huangxuanyuxiplus@gmail.com>
2021-09-17 17:37:36 +0800

    Update edit.py

--- fix-broken-double-redirect/edit.py
+++ fix-broken-double-redirect/edit.py
@@ -27,6 +27,10 @@ def fixPage(sourcePage):
     print(sourcePage.title())
     text = sourcePage.text
 
+    if re.search(r'{{\s*(Delete|Db-reason|D|Deletebecause|Db|速删|速刪|Speedy|SD|快删|快刪|CSD|QD)\s*(\|bot=Jimmy-bot)?\|g15(\||}})', text, flags=re.I):
+        print('\tnot g15')
+        return
+
     m = re.search(r'#(?:重定向|REDIRECT) ?\[\[(.+?)]]', text, flags=re.I)
     if m:
         middlePage = pywikibot.Page(site, m.group(1))
@@ -72,7 +76,4 @@ if __name__ == "__main__":
         cat = pywikibot.Page(site, cfg['csd_category'])
 
         for sourcePage in site.categorymembers(cat):
-            if re.search(r'{{\s*(Delete|Db-reason|D|Deletebecause|Db|速删|速刪|Speedy|SD|快删|快刪|CSD|QD)\s*(\|bot=Jimmy-bot)?\|g15(\||}})', sourcePage.text, flags=re.I):
-                print('\tnot g15')
-                continue
             fixPage(sourcePage)
