fb76da64f6835029cfe3f66b8c1ac5ac9d538b66
Xi-Plus <huangxuanyuxiplus@gmail.com>
2022-07-14 15:55:08 +0800

    Add toolforge files

---
+++ bootstrap_venv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# ref: https://wikitech.wikimedia.org/wiki/Help:Toolforge/Python#Kubernetes_python_jobs
+
+# use bash strict mode
+set -euo pipefail
+
+# create the venv
+python3 -m venv pyvenv
+
+# activate it
+source pyvenv/bin/activate
+
+# upgrade pip inside the venv and add support for the wheel package format
+pip install -U pip wheel
+
+# install some concrete packages
+pip install mwparserfromhell
+pip install pywikibot
