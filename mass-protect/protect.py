11cbc173127e75881add986b9321aa9b83d5492e
Xi-Plus <huangxuanyuxiplus@gmail.com>
2022-07-31 10:31:59 +0800

    Support other wikis

--- mass-protect/protect.py
+++ mass-protect/protect.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
+import argparse
 import csv
 import os
 
 os.environ['PYWIKIBOT_DIR'] = os.path.dirname(os.path.realpath(__file__))
 import pywikibot
 
+parser = argparse.ArgumentParser()
+parser.add_argument('lang', nargs='?', default='zh')
+parser.add_argument('wiki', nargs='?', default='wikipedia')
+parser.add_argument('dbwiki', nargs='?', default='zhwiki')
+args = parser.parse_args()
 
 os.environ['TZ'] = 'UTC'
 
-site = pywikibot.Site()
+site = pywikibot.Site(args.lang, args.wiki)
 site.login()
 
-with open('list.csv', 'r') as f:
+with open('list.csv', 'r', encoding='utf8') as f:
     r = csv.reader(f)
     for row in r:
         title = row[0]
