cff0b2d978dff10fca926a18173f885fa1851e45
xiplus <huangxuanyuxiplus@gmail.com>
2023-02-18 15:21:26 +0800

    Fix param1 counting

--- rail-interchange-count/main.py
+++ rail-interchange-count/main.py
@@ -5,14 +5,9 @@ from config import host, password, user  # pylint: disable=E0611,W0614
 
 
 query = '''
-SELECT pl_title, COUNT(*) AS cnt
-FROM (
-  SELECT LOWER(CONVERT(pl_title USING UTF8)) AS pl_title
-  FROM pagelinks
-  WHERE pl_namespace = 10 AND pl_title LIKE 'Rail-interchange/count/%'
-) t
-GROUP BY pl_title
-ORDER BY cnt DESC
+SELECT pl_from, pl_title
+FROM pagelinks
+WHERE pl_namespace = 10 AND pl_title LIKE 'Rail-interchange/count/%'
 '''
 
 conn = pymysql.connect(
@@ -26,13 +21,14 @@ with conn.cursor() as cursor:
     cursor.execute(query)
     result = cursor.fetchall()
 
-maincnt = defaultdict(int)
-subcnt = defaultdict(lambda: defaultdict(int))
+maincnt = defaultdict(set)
+subcnt = defaultdict(lambda: defaultdict(set))
 for row in result:
-    params = row[0].replace('rail-interchange/count/', '').split('-')
-    cnt = row[1]
-    maincnt[params[0]] += cnt
-    subcnt[params[0]]['-'.join(params[1:])] += cnt
+    pl_from = row[0]
+    pl_title = row[1].decode()
+    params = pl_title.replace('Rail-interchange/count/', '').lower().split('-')
+    maincnt[params[0]].add(pl_from)
+    subcnt[params[0]]['-'.join(params[1:])].add(pl_from)
 
 text = '''{| class="wikitable sortable" style="word-break: break-all;"
 ! param 1
@@ -42,14 +38,30 @@ text = '''{| class="wikitable sortable" style="word-break: break-all;"
 |-
 '''
 
+EXCLUDES = {
+    'cn',
+    'tw',
+    'taipei',
+    'london',
+    'gb',
+    'tokyo',
+    'kaohsiung',
+    'my',
+}
+allcnt = 0
+
 for key in sorted(maincnt.keys()):
     text += '| rowspan={} | {}\n'.format(len(subcnt[key]), key)
-    text += '| rowspan={} | {}\n'.format(len(subcnt[key]), maincnt[key])
+    text += '| rowspan={} | {}\n'.format(len(subcnt[key]), len(maincnt[key]))
     for subkey in sorted(subcnt[key].keys()):
         text += '| {}\n'.format(subkey)
-        text += '| {}\n'.format(subcnt[key][subkey])
+        text += '| {}\n'.format(len(subcnt[key][subkey]))
         text += '|-\n'
+    if key not in EXCLUDES:
+        allcnt += len(maincnt[key])
 text += '|}'
+
+print('allcnt', allcnt)
 
 with open('out.txt', 'w') as f:
     f.write(text)
