44f4ba26d34d4e1d7b4ec9bfe845ac1cb9133ecc
xiplus <huangxuanyuxiplus@gmail.com>
2023-02-23 16:10:37 +0800

    Add some args and fix redirect feature

--- move-pages/move.py
+++ move-pages/move.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
+import argparse
 import csv
 import os
+import re
 
 os.environ['PYWIKIBOT_DIR'] = os.path.dirname(os.path.realpath(__file__))
 import pywikibot
 
+parser = argparse.ArgumentParser()
+parser.add_argument('source')
+parser.add_argument('--no-redirect', action='store_true')
+parser.add_argument('--fix-redirect', action='store_true')
+parser.add_argument('--dry-run', action='store_true')
+args = parser.parse_args()
+print(args)
+
 site = pywikibot.Site()
 site.login()
 
-with open('list.csv', 'r') as f:
+with open(args.source, 'r', encoding='utf8') as f:
     reader = csv.reader(f)
     for row in reader:
         if len(row) < 3:
@@ -17,5 +27,18 @@ with open('list.csv', 'r') as f:
         newtitle = row[1].strip()
         reason = row[2]
         page = pywikibot.Page(site, oldtitle)
-        print('move', oldtitle, 'to', newtitle, 'reason', reason)
-        page.move(newtitle, reason=row[2], noredirect=True)
+        if args.fix_redirect:
+            redirects = [redirect.title() for redirect in page.redirects()]
+        print('move {} to {} reason {}'.format(oldtitle, newtitle, reason))
+        if not args.dry_run:
+            page.move(newtitle, reason=reason, noredirect=args.no_redirect)
+
+        if args.fix_redirect:
+            for redirect in redirects:
+                rdpage = pywikibot.Page(site, redirect)
+                if not rdpage.exists() or rdpage.title() == newtitle:
+                    continue
+                print('fix redirect {} to {}'.format(rdpage.title(), newtitle))
+                if not args.dry_run:
+                    rdpage.text = re.sub(r'#(?:REDIRECT|重定向)\s*\[\[.+?\]\]', '#REDIRECT [[' + newtitle + ']]', rdpage.text, flags=re.I)
+                    rdpage.save(reason, minor=True)
